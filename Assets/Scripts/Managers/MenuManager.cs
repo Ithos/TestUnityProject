32c016dfcbb50b7019298d946f2848733eb0d063
Ithos <alvaroperez.devel@gmail.com>
2016-01-25 01:09:57 +0100

    Initial menu scene.

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject[] buttonList;
+    public string[] tagList;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
